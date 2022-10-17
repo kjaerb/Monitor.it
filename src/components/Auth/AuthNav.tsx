7856003418c502c7738a3a231edc10125235d47b
kjaerb <kjaerb@gmail.com>
2022-10-17 13:31:13 +0200

    made frontpage auth mobile friendly

--- src/components/Auth/AuthNav.tsx
+++ src/components/Auth/AuthNav.tsx
@@ -1,39 +1,73 @@
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { useState } from 'react';
 
 import { ButtonMenu } from '@/components/ui/Button/ButtonMenu';
 import { AvatarImage } from '@/components/ui/Image/AvatarImage';
 
+import SidebarMobile from '../Sidebar/SidebarMobile';
+import SidebarMobileButton from '../Sidebar/SidebarMobileButton';
 import Button from '../ui/Button/Button';
 
 function AuthNav() {
   const { data: session } = useSession();
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div>
-      {session ? (
-        <>
-          <Button href={'/dashboard'}>Dashboard</Button>
+    <>
+      <SidebarMobile
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        side='right'
+      >
+        {}
+        <div>
+          {session ? (
+            <div className='flex flex-col w-full'>
+              <h1 className='flex flex-wrap font-bold text-lg mx-auto mb-4'>
+                Welcome {session.user?.name}
+              </h1>
+              <Button href={'/dashboard'}>Dashboard</Button>
+            </div>
+          ) : (
+            <>
+              <Button className='w-full' onClick={() => signIn('google')}>
+                Login
+              </Button>
+              <Button variant='light' className='w-full mt-4'>
+                Sign up
+              </Button>
+            </>
+          )}
+        </div>
+      </SidebarMobile>
+      <SidebarMobileButton setSidebarOpen={setSidebarOpen} />
 
-          <ButtonMenu
-            className='py-1'
-            items={[
-              {
-                children: 'Sign out',
-                onClick: () => signOut(),
-              },
-            ]}
-          >
-            <span className='font-bold mr-2'>{session.user?.name} </span>
-            <AvatarImage src={session.user?.image} width={30} height={30} />
-          </ButtonMenu>
-        </>
-      ) : (
-        // <Button href={"/login"}>Sign in</Button>
-        <Button href={'/dashboard'} onClick={() => signIn('github')}>
-          Sign in
-        </Button>
-      )}
-    </div>
+      <div className='hidden sm:block'>
+        {session ? (
+          <>
+            <Button href={'/dashboard'}>Dashboard</Button>
+
+            <ButtonMenu
+              className='py-1'
+              items={[
+                {
+                  children: 'Sign out',
+                  onClick: () => signOut(),
+                },
+              ]}
+            >
+              <span className='font-bold mr-2'>{session.user?.name} </span>
+              <AvatarImage src={session.user?.image} width={30} height={30} />
+            </ButtonMenu>
+          </>
+        ) : (
+          // <Button href={"/login"}>Sign in</Button>
+          <Button href={'/dashboard'} onClick={() => signIn('github')}>
+            Sign in
+          </Button>
+        )}
+      </div>
+    </>
   );
 }
 
--- src/components/Layouts/Header.tsx
+++ src/components/Layouts/Header.tsx
@@ -1,62 +1,10 @@
 import Link from 'next/link';
-import { useState } from 'react';
-
-import { navigation, teams } from '@/data/dashboardData';
 
 import AuthNav from '@/components/Auth/AuthNav';
-import {
-  SidebarMenuItem,
-  SidebarMenuItemIcon,
-} from '@/components/Sidebar/SidebarMenuItem';
-import SidebarMobile from '@/components/Sidebar/SidebarMobile';
-import SidebarMobileButton from '@/components/Sidebar/SidebarMobileButton';
 
 function LandingPageHeader() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
   return (
     <div className='max-w-4xl mx-auto py-3 z-1000 px-2 lg:px-0'>
-      <SidebarMobile
-        sidebarOpen={sidebarOpen}
-        setSidebarOpen={setSidebarOpen}
-        side='right'
-      >
-        <div className='space-y-1'>
-          {navigation.map((item, index) => (
-            <SidebarMenuItemIcon
-              key={index}
-              name={item.name}
-              href={item.href}
-              item={{
-                icon: item.icon,
-              }}
-              current={item.current}
-            />
-          ))}
-        </div>
-        <div className='mt-8'>
-          <h3
-            className='px-3 text-xs font-semibold text-gray-500 uppercase tracking-wider'
-            id='mobile-teams-headline'
-          >
-            Teams
-          </h3>
-          <div
-            className='mt-1 space-y-1'
-            role='group'
-            aria-labelledby='mobile-teams-headline'
-          >
-            {teams.map((team, index) => (
-              <SidebarMenuItem
-                key={index}
-                name={team.name}
-                href={team.href}
-                bgColorClass={team.bgColorClass}
-              />
-            ))}
-          </div>
-        </div>
-      </SidebarMobile>
       <nav className='flex justify-between items-center'>
         <Link href='/'>
           <span className='font-bold text-3xl cursor-pointer text-white'>
@@ -64,8 +12,7 @@ function LandingPageHeader() {
             <span className=' font-bold text-3xl cursor-pointer'>it</span>
           </span>
         </Link>
-        <SidebarMobileButton setSidebarOpen={setSidebarOpen} />
-        <div className='hidden sm:block'>
+        <div>
           <AuthNav />
         </div>
       </nav>
